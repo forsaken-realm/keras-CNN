39f5e9decbbb3313c50fbe0bd46c4fcc9352e025
forsaken-realm <64582408+forsaken-realm@users.noreply.github.com>
2020-05-24 12:37:46 +0530

    Update keras.py

--- keras.py
+++ keras.py
@@ -1,66 +1,57 @@
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-from keras.datasets import fashion_mnist
-from keras.models import save_model
-
-batch_size = 128
-num_classes = 10
-epochs = 12
-
-# input image dimensions
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Convolution2D, BatchNormalization, Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.datasets import fashion_mnist
+data = fashion_mnist.load_data()
+(x_train, y_train),(x_test,y_test) = data
 img_rows, img_cols = 28, 28
-
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
+y_train = tf.keras.utils.to_categorical(y_train,10 )
+y_test = tf.keras.utils.to_categorical(y_test,10)
 x_train /= 255
 x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
+model=Sequential()
+model.add(Convolution2D(filters=32, 
+                        kernel_size=(3,3), 
+                        activation='relu',
+                   input_shape=(28,28,1)
+                       ))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
-
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
+model.add(Dense(units=128, activation='relu'))
+model.add(Dense(units=10 , activation = 'softmax'))
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = tf.keras.optimizers.Adam(),
+              metrics = ['accuracy'])
+history = model.fit( x_train, y_train,
+          batch_size=128,
+          epochs=1,
           verbose=1,
           validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-filepath = './saved.h5'
+from tensorflow.keras.models import save_model
+filepath = '/hello/saved.h5'
 save_model(model ,filepath  )
-model1=load_model("saved.h5")
+accu = history.history['accuracy']
+f = open("accuracy.txt" , "w")
+f.write("%d" % accu)
+f.close()
+
+
+
+ 
+
+
+
+
